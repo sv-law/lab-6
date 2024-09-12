670fa27fccb45a740a16749507d81734e7323b91
Svasti <22300286@students.ltu.edu.au>
2024-09-13 01:45:09 +1000

    Exercises 3, 4 and 5

--- backend/querying2.js
+++ backend/querying2.js
@@ -46,11 +46,39 @@ Employees.findOne({ include: [Companies] }).then(employee => {
 
 // Exercise 3
 // *** TODO: Insert code here ***
+.then(() => console.log('# Peter Rabbit works for which company:'))
+.then(() => Employees.findOne({
+  where: 
+    {name: 'Peter Rabbit'}, 
+    include: [Companies]}
+  ))
+.then(employee => console.log(employee.company.dataValues))
 
 // Exercise 4
 // *** TODO: Insert code here ***
+.then(() => console.log('# Company with the highest profit and its employees:'))
+.then(() => Companies.findOne({
+  order: [['profit', 'DESC']], 
+  include: [Employees]
+}))
+.then(company => {
+  company.employees.forEach(employee => {
+    console.log(employee.dataValues);
+    console.log();
+  });
+})
 
 // Exercise 5
 // *** TODO: Insert code here ***  
+.then(() => Companies.findOne({id: 1}))
+.then(c => {
+  console.log('Inserting new employee to company ID =' + c.id);
+  const e3 = Employees.create({
+    name: 'Peter Junior',
+    age: 2,
+    companyId: c.id
+  })
+  return e3
+})
 
 .catch(console.error).then(() => db.close());
