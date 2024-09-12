66b65628bbaa59c57f6d2f33e9d6dc6d275f93b1
Svasti <22300286@students.ltu.edu.au>
2024-09-13 01:53:32 +1000

    Postgres config for connect_db and env file

--- backend/connect_db.js
+++ backend/connect_db.js
@@ -6,17 +6,18 @@
 const Sequelize = require('sequelize');
 
 const dbConfig = {
-  host: process.env.MYSQL_REMOTE_HOST,
-  port: process.env.MYSQL_REMOTE_PORT,
-  // here we are selecting mysql as the database type we will be using
-  dialect: 'mysql'
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  // // here we are selecting mysql as the database type we will be using
+  // now postgres
+  dialect: 'postgres'
 };
 
 // Here we connect to the database
 const db = new Sequelize(
   'development_db',
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASS,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   dbConfig);
 
 module.exports = db;
